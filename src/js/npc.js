4269145780f3dafd1312dd774d7940b18c27ce06
Mike Chelen <michael.chelen@gmail.com>
2014-11-18 20:27:36 -0500

    search results working

--- src/js/npc.js
+++ src/js/npc.js
@@ -6,17 +6,24 @@ $(document).ready(function () {
   require(["functions"]);
   require(["search"]);
 
-// catch click on sample search
-$(document).on('click', 'a.sample-search', function() {
-  // get sample search from text in <a> tag
-  var ssearch = $(this).text();
-  $("#querya").val(ssearch);
-  $("#querya").submit();
-});
+
+  // search form
+  $("form").on("submit", function (e) {
+    doSearch.call(this,e); 
+  });
+
+
+  // catch click on sample search
+  $(document).on('click', 'a.sample-search', function() {
+    // get sample search from text in <a> tag
+    var ssearch = $(this).text();
+    $("#querya").val(ssearch);
+    $("#querya").submit();
+  });
 
   // catch click on search results to show detailed page
-$(document).on('click', 'a.search-result', function() {
-  showResultDetail(mydata)
+  $(document).on('click', 'a.search-result', function() {
+    showResultDetail.call(this,mydata);
   }); 
   
 }); //close document.ready function
