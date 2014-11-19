e481be95aa3dbab60c51469f753360fa99effd73
Mike Chelen <mike@michaelchelen.net>
2014-11-19 14:03:39 -0500

    cleanup

--- src/js/showsearch.js
+++ src/js/showsearch.js
@@ -1,45 +1,41 @@
-function showSearchResults(bardata) {  
+function showSearchResults(mydata) {
 
-//  console.log(bardata);
+  $.each(mydata, function (i, entry) {
 
-  $.each(bardata, function (i, entry) {
-
-//  console.log(entry);
-
-  //create li
-  $listitem = $('<li>'); 
-  
-  //create link
-  $itemlink = $('<a>').attr({
-    "href":"#details-page",
-    "class":"search-result",
-    "id":plantid,
-    "data-transition":"slide"
-  }); 
-  
-  //create thumbnail image
-  $( "<img>").attr( "src", entry.thumb ).appendTo( $itemlink );
-  
-  // create html element with species name
-  $('<h3>', {text:entry.species}).appendTo( $itemlink ); 
-
-  // create html element with name
-  $('<p>', {text:entry.name+entry.commonNames}).appendTo( $itemlink );
-
-  // create html element plant type
-  $('<p>', {
-    text:"Plant Type: "+entry.plantTypes,
-    style:"font-style:italic"
-  }).appendTo( $itemlink );
+    //create li
+    $listitem = $('<li>'); 
     
-  // append link to list item
-  $itemlink.appendTo($listitem);   
-            
-  // append row to main list
-  $('#mylist').append($listitem);
-  
-  // trigger jquery mobile with newly built list
-  $("#mylist").listview("refresh");
+    //create link
+    $itemlink = $('<a>').attr({
+      "href":"#details-page",
+      "class":"search-result",
+      "id":plantid,
+      "data-transition":"slide"
+    }); 
+    
+    //create thumbnail image
+    $( "<img>").attr( "src", entry.thumb ).appendTo( $itemlink );
+    
+    // create html element with species name
+    $('<h3>', {text:entry.species}).appendTo( $itemlink ); 
+
+    // create html element with name
+    $('<p>', {text:entry.name+entry.commonNames}).appendTo( $itemlink );
+
+    // create html element plant type
+    $('<p>', {
+      text:"Plant Type: "+entry.plantTypes,
+      style:"font-style:italic"
+    }).appendTo( $itemlink );
+      
+    // append link to list item
+    $itemlink.appendTo($listitem);   
+              
+    // append row to main list
+    $('#mylist').append($listitem);
+    
+    // trigger jquery mobile with newly built list
+    $("#mylist").listview("refresh");
 
   });
 }
