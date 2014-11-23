56b298eb23632d8f8525d99011693e421b413b8b
Mike Chelen <michael.chelen@gmail.com>
2014-11-23 16:19:20 -0500

    cleanup, add object functions via prototype

--- src/js/npc.js
+++ src/js/npc.js
@@ -1,20 +1,14 @@
-
-
 $(document).ready(function () {   
 
-  var mydata; // create array to hold result data
-
-  var myobj = new npcsearch();
+  var myobj = new npcsearch(); // create instance of object
 
   // search form
   $("form").on("submit", function (e) {
     e.preventDefault(); // suppress form submit
     myobj.dosearch(this);
-    
   });
 
-
-  // catch click on sample search
+  // event handler for click on sample search
   $(document).on('click', 'a.sample-search', function() {
     // get sample search from text in <a> tag
     var ssearch = $(this).text();
@@ -22,11 +16,10 @@ $(document).ready(function () {
     $("#querya").submit();
   });
 
-  // create event handler for click on search results
+  // event handler for click on search results
   $(document).on('click', 'a.search-result', function() {
-    console.log($(this).attr('id'));
+//    console.log($(this).attr('id'));
     myobj.showResultDetail($(this).attr('id')); //send the clicked link id
   });
-
-  
+ 
 }); //close document.ready function
