179c0392038636ebb44686a33e73484842f12e85
Mike Chelen <michael.chelen@gmail.com>
2014-11-24 09:04:21 -0500

    add more processData() tests

--- test/tests.js
+++ test/tests.js
@@ -13,5 +13,8 @@ QUnit.test( "npcsearch()", function( assert ) {
 });
 
 QUnit.test( "processData()", function( assert ) {
-  assert.deepEqual(processData(queryData), goodProcessedData, "processData()" );
+  assert.deepEqual(processData(queryData), queryDataProcessedGood, "3 valid search results, keyword=mint" );
+  assert.deepEqual(processData(queryDataSingleResult), queryDataSingleResultProcessedGood, "1 valid search result, keyword=bittersweet" );
+  assert.equal(processData(zeroResultsQueryData),undefined,"zero search results");
+  assert.equal(processData(invalidQueryData),undefined,"invalid search result");
 });
