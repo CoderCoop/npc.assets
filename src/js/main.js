7ea627aa8848a201d3f9df9d9a541036c1005769
Mike Chelen <michael.chelen@gmail.com>
2014-11-27 23:17:45 -0500

    trying to use external node package (not working)

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,44 @@ $.mobile = require('jquery-mobile');
 
 
 
+
+//----------------------------
+
+
+
+
+
+$(document).ready(function () {   
+
+  var myobj = new npcsearch(); // create instance of object
+
+  // search form
+  $("form").on("submit", function (e) {
+    e.preventDefault(); // suppress form submit
+    myobj.dosearch(this);
+  });
+
+  // event handler for click on sample search
+  $(document).on('click', 'a.sample-search', function() {
+    // get sample search from text in <a> tag
+    var ssearch = $(this).text();
+    $("#querya").val(ssearch);
+    $("#querya").submit();
+  });
+
+  // event handler for click on search results
+  $(document).on('click', 'a.search-result', function() {
+//    console.log($(this).attr('id'));
+    myobj.showResultDetail($(this).attr('id')); //send the clicked link id
+  });
+ 
+}); //close document.ready function
+
+
+
+
+
+
 //----------------------------
 
 
@@ -255,39 +293,3 @@ function showSearchResults (mydata) {
 
 
 
-
-
-
-
-
-//----------------------------
-
-
-
-
-
-$(document).ready(function () {   
-
-  var myobj = new npcsearch(); // create instance of object
-
-  // search form
-  $("form").on("submit", function (e) {
-    e.preventDefault(); // suppress form submit
-    myobj.dosearch(this);
-  });
-
-  // event handler for click on sample search
-  $(document).on('click', 'a.sample-search', function() {
-    // get sample search from text in <a> tag
-    var ssearch = $(this).text();
-    $("#querya").val(ssearch);
-    $("#querya").submit();
-  });
-
-  // event handler for click on search results
-  $(document).on('click', 'a.search-result', function() {
-//    console.log($(this).attr('id'));
-    myobj.showResultDetail($(this).attr('id')); //send the clicked link id
-  });
- 
-}); //close document.ready function
